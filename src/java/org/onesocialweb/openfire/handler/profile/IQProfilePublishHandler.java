67bcc539047767e68bc825f46b2caae686854ddb
Laurent Eschenauer <laurent@eschenauer.be>
2010-04-19 11:30:56 +0200

    Fixed access control on various IQ get/set request (e.g. toString() issue on recipient)

--- src/java/org/onesocialweb/openfire/handler/profile/IQProfilePublishHandler.java
+++ src/java/org/onesocialweb/openfire/handler/profile/IQProfilePublishHandler.java
@@ -62,19 +62,28 @@ public class IQProfilePublishHandler extends IQHandler {
 		// error result packet
 		try {
 
-			// Only a local user can publish a profile update
-			if (!userManager.isRegisteredUser(sender.getNode())) {
+			// A valid request is an IQ of type set, 
+			if (!packet.getType().equals(IQ.Type.set)) {
+				IQ result = IQ.createResultIQ(packet);
+				result.setChildElement(packet.getChildElement().createCopy());
+				result.setError(PacketError.Condition.bad_request);
+				return result;
+			}
+			
+			// If a recipient is specified, it must be equal to the sender
+			// bareJID
+			if (recipient != null && !recipient.toString().equals(sender.toBareJID())) {
 				IQ result = IQ.createResultIQ(packet);
 				result.setChildElement(packet.getChildElement().createCopy());
 				result.setError(PacketError.Condition.not_authorized);
 				return result;
 			}
-
-			// A valid request is an IQ of type set, sent to the bare server
-			if ((!packet.getType().equals(IQ.Type.set) || (recipient != null && recipient.getNode() != null))) {
+			
+			// Only a local user can publish its profile
+			if (!userManager.isRegisteredUser(sender.getNode())) {
 				IQ result = IQ.createResultIQ(packet);
 				result.setChildElement(packet.getChildElement().createCopy());
-				result.setError(PacketError.Condition.bad_request);
+				result.setError(PacketError.Condition.not_authorized);
 				return result;
 			}
 
